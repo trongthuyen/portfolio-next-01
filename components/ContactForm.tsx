e857e7bdfc29fd128bd2e6b49b8fe49250e99a21
Tee <thuyen.nong-trong@banvien.com.vn>
2024-07-18 21:37:10 +0700

    fdd

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -7,7 +7,7 @@ const ContactForm = () => {
           type="text"
           placeholder="Your name"
           name="name"
-          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+          className="w-full px-3 py-2 text-sm text-gray-900 placeholder-gray-400 bg-white border-0 rounded shadow"
         />
       </div>
       <div className="mb-3">
@@ -15,14 +15,14 @@ const ContactForm = () => {
           type="email"
           placeholder="Email"
           name="email"
-          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+          className="w-full px-3 py-2 text-sm text-gray-900 placeholder-gray-400 bg-white border-0 rounded shadow"
         />
       </div>
       <div className="mb-3">
         <input
           placeholder="Your message"
           name="message"
-          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+          className="w-full px-3 py-2 text-sm text-gray-900 placeholder-gray-400 bg-white border-0 rounded shadow"
         />
       </div>
       <button className="px-6 py-3 mb-5 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 hover:bg-blue-400">
